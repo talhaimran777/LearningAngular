0d909cf63db6130eb811583a62d1da62e7336265
Talha Imran <talhaimran284@gmail.com>
2020-12-01 13:27:33 +0500

    Added a nav link to heroes text

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -8,15 +8,21 @@ import Hero from '../../interfaces/Hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   constructor() {
-    this.heroes = [];
+    this.heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+    ];
   }
 
-  ngOnInit(): void {
-    this.addHero();
-    this.addHero();
-    this.addHero();
-    this.addHero();
-  }
+  ngOnInit(): void {}
 
   addHero = () => {
     this.heroes.push({
