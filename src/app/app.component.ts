6e81a8a8469eaed8ec1a0eb12441f5ba25a42112
Talha Imran <talhaimran284@gmail.com>
2020-12-04 20:26:53 +0500

    Completed first app string converter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'learning-angular';
+  changedString: string;
+  enteredString: string;
+  constructor() {
+    this.changedString = '';
+    this.enteredString = '';
+  }
+
+  onChange = (e: any) => {
+    this.enteredString = e.target.value;
+  };
+
+  onClick = (e: any) => {
+    this.changedString = this.enteredString.toUpperCase();
+  };
 }
